9c124138a59c3e4268526fe026acf75b602758f9
Aldo Iljazi <ylrxeidx@openmailbox.org>
2015-03-26 14:10:44 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 # Download elementary-script
+mkdir ~/tmp/
 wget -q -O ~/tmp/elementary-script.sh https://raw.githubusercontent.com/ylrxeidx/elementary-script/master/elementary-script.sh
 
 # Run elementary-script
-~/tmp/elementary-script.sh
+sh ~/tmp/elementary-script.sh
+rm -rf ~/tmp/
