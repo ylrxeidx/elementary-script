a603a335194b2b0ea47cef529e82c44e4978c504
Aldo Iljazi <ylrxeidx@openmailbox.org>
2015-03-26 14:16:40 +0200

    Delete run.sh

--- run.sh
+++
@@ -1,7 +0,0 @@
-# Download elementary-script
-mkdir ~/tmp/
-wget -q -O ~/tmp/elementary-script.sh https://raw.githubusercontent.com/ylrxeidx/elementary-script/master/elementary-script.sh
-
-# Run elementary-script
-sh ~/tmp/elementary-script.sh
-rm -rf ~/tmp/
