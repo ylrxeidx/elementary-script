8d5dbe4db6ae7d2fbe1f177944fe909c274da684
btd1337 <helder.bertoldo@ice.ufjf.br>
2016-12-04 19:59:08 -0200

    0.1 - Update Installer

--- elementary-script.sh
+++ elementary-script.sh
@@ -3,14 +3,16 @@ clear
 
 # Zenity
 GUI=$(zenity --list --checklist \
-	--height 400 \
-	--width 800 \
+	--height 500 \
+	--width 900 \
 	--title="elementary-script" \
 	--text "Pick one or multiple Actions to execute." \
 	--column=Picks \
 	--column=Actions \
 	--column=Description \
-	FALSE "Update System" "Updates the package lists, the system packages and Applications."  \
+	TRUE "Update System" "Updates the package lists, the system packages and Applications."  \
+	TRUE "Enable PPAs" "Another extra layer of security and another level of annoyance. You cannot add PPA by default in Loki." \
+	FALSE "Install Elementary Tweaks" "Installing themes in elementary OS is a much easier task thanks to elementary Tweaks tool." \
 	FALSE "Install Proprietary Drivers" "Installs the proprietary drivers."  \
 	FALSE "Speed-Up Memory" "Installs preload and enables zRAM." \
 	FALSE "Install Ubuntu Restricted Extras" "Installs commonly used applications with restricted copyright (mp3, avi, mpeg, TrueType, Java, Flash, Codecs)." \
@@ -23,12 +25,17 @@ GUI=$(zenity --list --checklist \
 	FALSE "Install Firefox" "Installs Firefox. A free and open-source web browser." \
 	FALSE "Install Liferea" "Installs Liferea. a web feed reader/news aggregator that brings together all of the content from your favorite subscriptions into a simple interface that makes it easy to organize and browse feeds. Its GUI is similar to a desktop mail/newsclient, with an embedded graphical browser." \
 	FALSE "Install VLC" "Installs VLC. A free and open source cross-platform multimedia player and framework that plays most multimedia files as well as DVDs, Audio CDs, VCDs, and various streaming protocols." \
+	FALSE "Install Clementine Music Player" "Installs Clementine. One of the Best Music Players and library organizer on Linux." \
 	FALSE "Install Transmission" "Installs the Transmission BitTorrent client." \
 	FALSE "Install Atom" "Installs Atom. A hackable text editor for the 21st Century." \
 	FALSE "Install Sublime Text 3" "Installs Sublime Text 3. A sophisticated text editor for code, markup and prose." \
 	FALSE "Install LibreOffice" "Installs LibreOffice. A powerful office suite." \
+	FALSE "Install WPS Office" "Installs WPS Office. The most compatible free office suite." \
+	FALSE "Install TLP" "Install TLP to save battery and prevent overheating" \
+	FALSE "Install Redshift" "Use night shift to save your eyes" \
 	FALSE "Fix Broken Packages" "Fixes the broken packages." \
 	FALSE "Clean-Up Junk" "Removes unnecessary packages and the local repository of retrieved package files." \
+	FALSE "Enable Plank magnifying effect" "Enabling OSX-style zoom in Plank" \
 	--separator=', ');
 
 # Update System Action
@@ -37,8 +44,28 @@ then
 	clear
 	echo "Updating system..."
 	echo ""
-	sudo apt-get -y update
-	sudo apt-get -y upgrade
+	sudo apt -y update
+	sudo apt -y full-upgrade
+fi
+
+# Enable PPAs
+if [[ $GUI == *"Enable PPAs"* ]]
+then
+	clear
+	echo "Enabling PPAs..."
+	echo ""
+	sudo apt -y install software-properties-common
+fi
+
+# Install Elementary Tweaks
+if [[ $GUI == *"Install Elementary Tweaks"* ]]
+then
+	clear
+	echo "Installing Elementary Tweaks..."
+	echo ""
+	sudo add-apt-repository -y ppa:philip.scott/elementary-tweaks
+	sudo apt update
+	sudo apt -y install elementary-tweaks
 fi
 
 # Install Proprietary Drivers Action
@@ -56,8 +83,8 @@ then
 	clear
 	echo "Speeding-up Memory..."
 	echo ""
-	sudo apt-get -y install preload
-	sudo apt-get -y install zram-config
+	sudo apt -y install preload
+	sudo apt -y install zram-config
 fi
 
 # Install Ubuntu Restricted Extras Action
@@ -66,7 +93,7 @@ then
 	clear
 	echo "Installing Ubuntu Restricted Extras..."
 	echo ""
-	sudo apt-get -y install ubuntu-restricted-extras
+	sudo apt -y install ubuntu-restricted-extras
 fi
 
 # Install Extra Multimedia Codecs Action
@@ -75,7 +102,7 @@ then
 	clear
 	echo "Installing Extra Multimedia Codecs..."
 	echo ""
-	sudo apt-get -y install libavcodec-extra-53
+	sudo apt -y install libavcodec-extra-53
 fi
 
 # Install Support for Encrypted DVD's Action
@@ -84,7 +111,7 @@ then
 	clear
 	echo "Installing Support for Encrypted DVD's..."
 	echo ""
-	sudo apt-get -y install libdvdread4
+	sudo apt -y install libdvdread4
 	sudo /usr/share/doc/libdvdread4/install-css.sh
 fi
 
@@ -94,7 +121,7 @@ then
 	clear
 	echo "Installing Support for Archive Formats"
 	echo ""
-	sudo apt-get -y install zip unzip p7zip p7zip-rar rar unrar
+	sudo apt -y install zip unzip p7zip p7zip-rar rar unrar
 fi
 
 # Install GDebi Action
@@ -103,7 +130,7 @@ then
 	clear
 	echo "Installing GDebi..."
 	echo ""
-	sudo apt-get -y install gdebi
+	sudo apt -y install gdebi
 fi
 
 # Install Google Chrome Action
@@ -129,7 +156,7 @@ then
 	clear
 	echo "Installing Chromium..."
 	echo ""
-	sudo apt-get -y install chromium-browser
+	sudo apt -y install chromium-browser
 fi
 
 # Install Firefox Action
@@ -138,7 +165,7 @@ then
 	clear
 	echo "Installing Firefox..."
 	echo ""
-	sudo apt-get -y install firefox
+	sudo apt -y install firefox
 fi
 
 # Install Liferea Action
@@ -147,7 +174,7 @@ then
 	clear
 	echo "Installing Liferea..."
 	echo ""
-	sudo apt-get -y install liferea
+	sudo apt -y install liferea
 fi
 
 # Install VLC Action
@@ -156,7 +183,16 @@ then
 	clear
 	echo "Installing VLC..."
 	echo ""
-	sudo apt-get -y install vlc
+	sudo apt -y install vlc
+fi
+
+# Install Clementine Action
+if [[ $GUI == *"Install Clementine Music Player"* ]]
+then
+	clear
+	echo "Installing Clementine Music Player..."
+	echo ""
+	sudo apt -y install clementine
 fi
 
 # Install Transmission Action
@@ -165,7 +201,7 @@ then
 	clear
 	echo "Installing Transmission..."
 	echo ""
-	sudo apt-get -y install transmission
+	sudo apt -y install transmission
 fi
 
 # Install Atom Action
@@ -175,8 +211,8 @@ then
 	echo "Installing Atom..."
 	echo ""
 	sudo add-apt-repository -y ppa:webupd8team/atom
-	sudo apt-get -y update
-	sudo apt-get -y install atom
+	sudo apt -y update
+	sudo apt -y install atom
 fi
 
 # Install Sublime Text 3 Action
@@ -186,8 +222,8 @@ then
 	echo "Installing Sublime Text 3..."
 	echo ""
 	sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
-	sudo apt-get -y update
-	sudo apt-get -y install sublime-text-installer
+	sudo apt -y update
+	sudo apt -y install sublime-text-installer
 fi
 
 # Install LibreOffice Action
@@ -196,7 +232,49 @@ then
 	clear
 	echo "Installing LibreOffice..."
 	echo ""
-	sudo apt-get -y install libreoffice
+	sudo apt -y install libreoffice
+fi
+
+# Install WPS Office
+if [[ $GUI == *"Install WPS Office"* ]]
+then
+	clear
+	echo "Installing WPS Office..."
+	echo ""
+	if [[ $(uname -m) == "i686" ]]
+	then
+		wget -O /tmp/wps-office_10.1.0.5672~a21_i386.deb http://kdl.cc.ksosoft.com/wps-community/download/a21/wps-office_10.1.0.5672~a21_i386.deb
+		sudo dpkg -i /tmp/wps-office_10.1.0.5672~a21_i386.deb
+	elif [[ $(uname -m) == "x86_64" ]]
+	then
+		wget -O /tmp/wps-office_10.1.0.5672~a21_amd64.deb http://kdl.cc.ksosoft.com/wps-community/download/a21/wps-office_10.1.0.5672~a21_amd64.deb
+		sudo dpkg -i /tmp/wps-office_10.1.0.5672~a21_amd64.deb
+	fi
+	#Fonts, Interface Translate, Dictionary
+	wget -O /tmp/wps-office-fonts_1.0_all.deb http://kdl.cc.ksosoft.com/wps-community/download/fonts/wps-office-fonts_1.0_all.deb
+	wget -O /tmp/wps-office-ul_10.1.0.5503-0kaiana05052016_all.deb http://repo.uniaolivre.com/packages/xenial/wps-office-ul_10.1.0.5503-0kaiana05052016_all.deb
+	wget -O /tmp/wps-office-language-all_0.1_all.deb https://doc-0k-5g-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/vmsics07sveefmft458910ml3prvahpt/1480881600000/05316569172087402966/*/0B7HGeEB4kyvMaU5SbkdRRjBYWHc?e=download
+	sudo dpkg -i /tmp/wps-office-fonts_1.0_all.deb
+	sudo dpkg -i /tmp/wps-office-ul_10.1.0.5503-0kaiana05052016_all.deb
+	sudo dpkg -i /tmp/wps-office-language-all_0.1_all.deb
+fi
+
+# Install TLP
+if [[ $GUI == *"Install TLP"* ]]
+then
+	clear
+	echo "Installing TLP..."
+	echo ""
+	sudo apt -y install tlp tlp-rdw
+fi
+
+# Install Redshift
+if [[ $GUI == *"Install Redshift"* ]]
+then
+	clear
+	echo "Installing Redshift..."
+	echo ""
+	sudo apt -y install redshift
 fi
 
 # Fix Broken Packages Action
@@ -205,7 +283,7 @@ then
 	clear
 	echo "Fixing the broken packages..."
 	echo ""
-	sudo apt-get -y -f install
+	sudo apt -y -f install
 fi
 
 # Clean-Up Junk Action
@@ -214,8 +292,20 @@ then
 	clear
 	echo "Cleaning-up junk..."
 	echo ""
-	sudo apt-get -y autoremove
-	sudo apt-get -y autoclean
+	sudo apt -y autoremove
+	sudo apt -y autoclean
+fi
+
+# Install Enable Plank magnifying effect
+if [[ $GUI == *"Enable Plank magnifying effect"* ]]
+then
+	clear
+	echo "Enabling Plank magnifying effect..."
+	echo ""
+	sudo apt --purge remove -y plank
+	sudo add-apt-repository -y ppa:ricotz/docky
+	sudo apt update
+	sudo apt -y install plank
 fi
 
 # Notification
