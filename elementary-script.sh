3a4d3d350f269ad5dc763885995dc9a5b549eb39
btd1337 <helder.bertoldo@ice.ufjf.br>
2016-12-05 01:58:15 -0200

    Increase dropbox support

--- elementary-script.sh
+++ elementary-script.sh
@@ -211,6 +211,7 @@ then
 	echo ""
 	sudo apt -y install git
 	sudo apt --purge remove -y dropbox*
+	sudo apt -y install python-gpgme	
 	git clone https://github.com/zant95/elementary-dropbox /tmp/elementary-dropbox
 	sudo bash /tmp/elementary-dropbox/install.sh
 fi
